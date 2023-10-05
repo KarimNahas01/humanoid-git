88c6a9ff85ec7e7c8b72e7532dd40138f125f9ae
Róbert Fjölnir Birkisson <rfb2@hi.is>
2023-10-05 15:13:19 +0200

    Added arduino code to read Serial

---
+++ read_serial/read_serial.ino
@@ -0,0 +1,23 @@
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+
+}
+
+int servoValues[5] = {0,0,0,0,0};
+String inStr = "";
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  while (Serial.available() == 0) {} // Wait for data
+  inStr = Serial.readString();
+  inStr.trim();
+
+  int values[5];
+  for (int i = 0; i < 5; i++) {
+    String num = inStr.substring(2*i, 2*i+1);
+    values[i] = num.toInt();
+  }
+
+  // Now we need to convert the percentage values to servo values
+}
